d000a0788f57ca1cfc77f2f1398d68c13a1c538e
vladhusar <vladgusar23@gmail.com>
2024-03-02 19:46:51 +0200

    finish animation

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   list.classList.toggle("active");
 });
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  treshhold: 0.1,
+};
+function callback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isInterSection) {
+      console.log("Елемнт видимий");
+    } else {
+      console.log("Елемент не видимий");
+    }
+  });
+}
+const observer = new IntersectionObserver(callback, options);
+const targetElement = document.querySelector(".customers__list");
+console.log(targetElement);
+observer.observe(targetElement);
